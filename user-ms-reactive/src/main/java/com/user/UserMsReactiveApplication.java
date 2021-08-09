2d0ea51721f499750063dbd2d8a3561197db1a92
ketan-gote <ketan.gote@gmail.com>
2021-08-09 15:08:39 +0530

    Changes: Fetch data from config server..

--- user-ms-reactive/src/main/java/com/user/UserMsReactiveApplication.java
+++ user-ms-reactive/src/main/java/com/user/UserMsReactiveApplication.java
@@ -1,11 +1,11 @@
 package com.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class UserMsReactiveApplication {
 
 	public static void main(String[] args) {
