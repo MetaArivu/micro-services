a19ff9e4692e323dfe6a6118f62e26ab625f8b12
ketangote <ketan.gote@metamagic.in>
2021-08-18 09:33:29 +0530

    Cart cmd & query service integration with euerka and api gateway

--- cart-service/cart.query.api/src/main/java/com/cart/CartQueryApplication.java
+++ cart-service/cart.query.api/src/main/java/com/cart/CartQueryApplication.java
@@ -2,11 +2,17 @@ package com.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import com.cart.core.config.DBConfiguration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Cart Query Service Documentation APIs v1.0"))
 @Import({ DBConfiguration.class })
 public class CartQueryApplication {
 
